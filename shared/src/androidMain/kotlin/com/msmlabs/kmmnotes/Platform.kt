836f8d2d0c6a070374c3e0a207deaa3a41f218ae
Matheus Miranda <matheus.sm86@gmail.com>
2022-10-16 00:29:01 -0300

    chore: remove unused files

--- shared/src/androidMain/kotlin/com/msmlabs/kmmnotes/Platform.kt
+++
@@ -1,7 +0,0 @@
-package com.msmlabs.kmmnotes
-
-class AndroidPlatform : Platform {
-    override val name: String = "Android ${android.os.Build.VERSION.SDK_INT}"
-}
-
-actual fun getPlatform(): Platform = AndroidPlatform()
--- shared/src/commonMain/kotlin/com/msmlabs/kmmnotes/Platform.kt
+++
@@ -1,7 +0,0 @@
-package com.msmlabs.kmmnotes
-
-interface Platform {
-    val name: String
-}
-
-expect fun getPlatform(): Platform
