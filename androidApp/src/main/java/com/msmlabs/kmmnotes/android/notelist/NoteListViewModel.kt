4f487205892a9f44462f2c407834fb4819cfeac4
Matheus Miranda <matheus.sm86@gmail.com>
2022-10-16 16:23:56 -0300

    feat: create NoteListViewModel

---
+++ androidApp/src/main/java/com/msmlabs/kmmnotes/android/notelist/NoteListViewModel.kt
@@ -0,0 +1,64 @@
+package com.msmlabs.kmmnotes.android.notelist
+
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.msmlabs.kmmnotes.domain.note.Note
+import com.msmlabs.kmmnotes.domain.note.NoteDataSource
+import com.msmlabs.kmmnotes.domain.note.SearchNoteUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+private const val NOTES_KEY = "notes"
+private const val SEARCH_TEXT_KEY = "searchText"
+private const val SEARCH_ACTIVE_KEY = "searchActive"
+private const val TIMEOUT_MILLIS = 5000L
+
+@HiltViewModel
+class NoteListViewModel @Inject constructor(
+    private val noteDataSource: NoteDataSource,
+    private val savedStateHandle: SavedStateHandle,
+) : ViewModel() {
+
+    private val searchNotesUseCase = SearchNoteUseCase()
+
+    private val notes = savedStateHandle.getStateFlow(NOTES_KEY, emptyList<Note>())
+    private val searchText = savedStateHandle.getStateFlow(SEARCH_TEXT_KEY, "")
+    private val isSearchActive = savedStateHandle.getStateFlow(SEARCH_ACTIVE_KEY, false)
+
+    val state = combine(notes, searchText, isSearchActive) { notes, searchText, isSearchActive ->
+        NoteListState(
+            notes = searchNotesUseCase(notes, searchText),
+            searchText = searchText,
+            isSearchActive = isSearchActive
+        )
+    }.stateIn(viewModelScope, SharingStarted.WhileSubscribed(TIMEOUT_MILLIS), NoteListState())
+
+    fun loadNotes() {
+        viewModelScope.launch {
+            savedStateHandle[NOTES_KEY] = noteDataSource.getAllNotes()
+        }
+    }
+
+    fun onSearchTextChange(text: String) {
+        savedStateHandle[SEARCH_TEXT_KEY] = text
+    }
+
+    fun onToggleSearch() {
+        savedStateHandle[SEARCH_ACTIVE_KEY] = !isSearchActive.value
+        if (!isSearchActive.value) {
+            savedStateHandle[SEARCH_TEXT_KEY] = ""
+        }
+    }
+
+    fun deleteNoteById(id: Long) {
+        viewModelScope.launch {
+            noteDataSource.deleteNoteById(id)
+            loadNotes() // must reload notes again since we're not using Flow
+        }
+    }
+}
